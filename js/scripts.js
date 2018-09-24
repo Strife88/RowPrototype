7fbc5105a95f3ef8dc6e88d4e398c68d1ed5234f
alexander <strife88@mail.ru>
2018-09-25 09:01:52 +1000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//need to make 3 rows
+//on row click - expand row
+//on row click - change image to variable
+//on each iteration - change variable
+
+//creation of object
+
+var createTag = function(tag,classname,parent,id,text) {
+ var element = document.createElement(tag);
+ if(classname) {
+ element.classList.add(classname);
+ }
+ if(text){
+ element.innerHTML = text;
+ }
+ if(id){  
+ element.setAttribute("id", id);     
+ }
+ var container = document.getElementsByClassName(parent);
+ for(i=0;i< container.length;i++) {
+ container[i].appendChild(element);
+ }
+};
+
+
+    console.log('shit');
+    createTag('div','row','column','row-1');
