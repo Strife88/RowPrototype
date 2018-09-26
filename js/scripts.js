08961253c4353c56148cf7abbaf4d3ba015f4a67
Alexander <strife88@mail.ru>
2018-09-26 11:15:46 +1000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ for(i=1;i<9;i++){
     createTag('div','row','column','row-'+i);
     var currentSelector = document.getElementById("row-"+i);
 //addImage based on iterator
-    currentSelector.style.background = "url('../images/Card-Normal-"+ i +".png')";
+    currentSelector.style.background = "url('../RowPrototype/images/Card-Normal-"+ i +".png')";
 }
 
 var cardContainer = document.getElementById("container");
@@ -30,10 +30,10 @@ for (var i = 0; i < btns.length; i++) {
 btns[i].addEventListener("click", function() {
   this.classList.toggle("active");
     if(this.classList == "row active"){
-    this.style.background = "url('../images/Card-Expanded-"+ this.getAttribute("id").slice(-1) +".png')";
+    this.style.background = "url('../RowPrototype/images/Card-Expanded-"+ this.getAttribute("id").slice(-1) +".png')";
     }
     else {
-    this.style.background = "url('../images/Card-Normal-"+ this.getAttribute("id").slice(-1) +".png')";    
+    this.style.background = "url('../RowPrototype/images/Card-Normal-"+ this.getAttribute("id").slice(-1) +".png')";    
 }
 }
 );
